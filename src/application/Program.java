5ce5a1d178ce3679bfb30b314b6dd389ecf33cb1
Alberto Rego <betoprogramer@gmail.com>
2021-04-18 21:14:13 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		System.out.println("=== Test 1: seller findById ===");
@@ -44,6 +46,15 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerdao.update(seller);
 		System.out.println("Update  completed");
+		
+		System.out.println("\n=== Test 6: seller Delete===");
+		
+		System.out.println("Enter id for delete test");
+		int id = sc.nextInt();
+		sellerdao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 		
 		
 	}
--- src/model/dao/mpl/SellerDaoJDBC.java
+++ src/model/dao/mpl/SellerDaoJDBC.java
@@ -64,7 +64,7 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void update(Seller obj) {
-PreparedStatement st = null;
+		PreparedStatement st = null;
 		
 		try {
 			st = conn.prepareStatement("UPDATE seller "
@@ -95,7 +95,21 @@ PreparedStatement st = null;
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ? ");
+			st.setInt(1, id);
+			int rows = st.executeUpdate();
+			if(rows == 0) {
+				System.out.println("Id not exist");
+			}
+		} catch (SQLException e) {
+			
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
